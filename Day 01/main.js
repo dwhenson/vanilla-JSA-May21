e6ae6b6b5c9f5fee33e1933696a2dca2adceeedb
dwhenson <dave.henson@gmail.com>
2021-05-31 12:14:42 +0300

    ☑️ ADD: Basic JS toggle solution

--- Day 01/main.js
+++ Day 01/main.js
@@ -1,1 +1,22 @@
-console.log("it works");
+/* ====================================================
+   Variables
+   ==================================================== */
+const passwordInput = document.querySelector("#password");
+const showPassword = document.querySelector("#show-password");
+const submitButton = document.querySelector("button");
+
+/* ====================================================
+   Functions
+   ==================================================== */
+function togglePassword() {
+	if (passwordInput.type === "password") {
+		passwordInput.type = "text";
+	} else {
+		passwordInput.type = "password";
+	}
+}
+
+/* ====================================================
+   Inits and Event Listeners
+   ==================================================== */
+showPassword.addEventListener("change", togglePassword);
