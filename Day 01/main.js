baef38be1e24607d7260831f1784f9850f1d3d62
dwhenson <dave.henson@gmail.com>
2021-05-31 16:41:50 +0300

    ☑️ ADD: Submission verification and text for screen readers

--- Day 01/main.js
+++ Day 01/main.js
@@ -1,22 +1,71 @@
 /* ====================================================
    Variables
    ==================================================== */
+const form = document.querySelector("form");
 const passwordInput = document.querySelector("#password");
-const showPassword = document.querySelector("#show-password");
 const submitButton = document.querySelector("button");
 
 /* ====================================================
    Functions
    ==================================================== */
-function togglePassword() {
+
+/**
+ * Render HTML that is dependent on JavaScript
+ */
+function renderPasswordToggle() {
+	const toggleControls = document.createElement("label");
+	toggleControls.innerHTML = `
+		<input type="checkbox" name="show-passwords" id="show-password" />
+			Show password
+		<span id="visibility-state" class="visually-hidden" aria-live="polite"></span>
+		`;
+	passwordInput.after(toggleControls);
+}
+
+/**
+ * Update the password status message for screen readers
+ * @param      {object}  element  The element that contains the text message
+ */
+function updatePasswordStatus(element) {
+	if (passwordInput.type === "text") {
+		element.textContent = "Your password is visible";
+	}
+	if (passwordInput.type === "password") {
+		element.textContent = "Your password is hidden";
+	}
+}
+
+/**
+ * Toggle the visibility of the password input
+ * @param      {event}  event   The event object
+ */
+function togglePassword(event) {
+	if (!event.target.matches("#show-password")) return;
 	if (passwordInput.type === "password") {
 		passwordInput.type = "text";
 	} else {
 		passwordInput.type = "password";
 	}
+	// Get the element that provides status updates for screen readers
+	const element = form.querySelector("#visibility-state");
+	updatePasswordStatus(element);
+}
+
+/**
+ * Check if the input is a password and submit the form
+ * @param      {event}  event   The event object
+ */
+function submitHandler(event) {
+	event.preventDefault();
+	if (passwordInput.type === "text") {
+		passwordInput.type = "password";
+	}
+	form.submit();
 }
 
 /* ====================================================
    Inits and Event Listeners
    ==================================================== */
-showPassword.addEventListener("change", togglePassword);
+renderPasswordToggle();
+form.addEventListener("change", togglePassword);
+submitButton.addEventListener("click", submitHandler);
