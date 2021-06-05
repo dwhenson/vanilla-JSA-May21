b28bb972874c923b8570d121e1924732c743dfca
dwhenson <dave.henson@gmail.com>
2021-06-05 16:55:39 +0300

    🐛 FIX: All file and folder names to correct course days

--- Day 03/main.js
+++ Day 03/main.js
@@ -1,29 +1,23 @@
 /* ====================================================
    Variables
    ==================================================== */
-// const passwordInput = document.querySelector("#new-password");
-const passwordInputs = [...document.querySelectorAll("[type='password']")];
+const form = document.querySelector("form");
+const passwordInput = document.querySelector("#new-password");
+const inputs = document.querySelectorAll("[type='password']");
 const submitButton = document.querySelector("button");
+let passwordToggle;
 
 /* ====================================================
    Functions
    ==================================================== */
 
 /**
- * Check if relevant form inputs are a password, if not convert and submit the form
+ * Check if the input is a password, if not convert and submit the form
  * @param      {event}  event   The event object
  */
 function submitHandler(event) {
 	event.preventDefault();
-	const form = event.target.closest("form");
-	// Get all relevant inputs inside the relevant form
-	const inputs = form.querySelectorAll("[type='text']");
-	// Convert back to password as needed before form submission
-	if (inputs.length >= 1) {
-		inputs.forEach(function (input) {
-			input.type = "password";
-		});
-	}
+	passwordInput.type = "password";
 	form.submit();
 }
 
@@ -31,10 +25,11 @@ function submitHandler(event) {
  * Update the password status message for screen readers
  * @param      {object}  element  The element that contains the text message
  */
-function updatePasswordStatus(event, element) {
-	if (event.target.checked) {
+function updatePasswordStatus(element) {
+	if (passwordInput.type === "text") {
 		element.textContent = "Your password is visible";
-	} else {
+	}
+	if (passwordInput.type === "password") {
 		element.textContent = "Your password is hidden";
 	}
 }
@@ -44,44 +39,37 @@ function updatePasswordStatus(event, element) {
  * @param      {event}  event   The event object
  */
 function togglePassword(event) {
-	if (!event.target.matches("[type='checkbox']")) return;
-	const form = event.target.closest("form");
-	passwordInputs.forEach(function (passwordInput) {
-		// If the checkbox and password input aren't in the same form return
-		if (form !== passwordInput.closest("form")) return;
-		// If so convert type to show password
-		if (event.target.checked) {
-			passwordInput.type = "text";
+	if (event.target !== passwordToggle) return;
+	// Loop over inputs and adjust type based on checkbox state
+	inputs.forEach(function (input) {
+		if (passwordToggle.checked) {
+			input.type = "text";
 		} else {
-			passwordInput.type = "password";
+			input.type = "password";
 		}
 	});
 	// Get the element that updates status for screen readers, and call update
-	const element = form.querySelector(".visually-hidden");
-	updatePasswordStatus(event, element);
+	const element = form.querySelector("#visibility-state");
+	updatePasswordStatus(element);
 }
 
 /**
  * Render HTML that is dependent on JavaScript
  */
 function renderPasswordToggle() {
-	// Get the final input elements
-	const lastInputs = document.querySelectorAll("div:last-of-type input");
-	// Render the show password checkbox after each final input element
-	lastInputs.forEach(function (input, index) {
-		const toggleControls = document.createElement("label");
-		toggleControls.innerHTML = `
-			<input type="checkbox" name="show-passwords" id="show-password-${index}" />
-				Show password
-			<span class="visually-hidden" aria-live="polite"></span>
-			`;
-		input.after(toggleControls);
-	});
+	const toggleControls = document.createElement("label");
+	toggleControls.innerHTML = `
+		<input type="checkbox" name="show-passwords" id="show-password" />
+			Show password
+		<span id="visibility-state" class="visually-hidden" aria-live="polite"></span>
+		`;
+	passwordInput.after(toggleControls);
+	passwordToggle = document.querySelector("#show-password");
 }
 
 /* ====================================================
    Inits and Event Listeners
    ==================================================== */
 renderPasswordToggle();
-document.addEventListener("change", togglePassword);
+form.addEventListener("change", togglePassword);
 submitButton.addEventListener("click", submitHandler);
