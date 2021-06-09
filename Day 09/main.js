e1f37bc9a63979781a1ff0097121a90c844bcc68
dwhenson <dave.henson@gmail.com>
2021-06-09 17:39:18 +0300

    ✨ IMPROVE: add correct singular/plural words

--- Day 09/main.js
+++ Day 09/main.js
@@ -4,25 +4,38 @@
 const textarea = document.querySelector("#text");
 const charCount = document.querySelector("#character-count");
 const wordCount = document.querySelector("#word-count");
-const wordCount = document.querySelector("#word-count");
-const regex = /[\n\r\s]+/g
 
 /* ====================================================
 	Functions
 	==================================================== */
 
+/**
+ * Counts the number of words.
+ * @param      {String}  string  The string of text content
+ * @return     {Number}  Number of words.
+ */
 function countWords(string) {
-	const words = string.split(regex)
-	const longWords = words.filter(function(word) {
-		return word.length > 0;
-	})
-	return words.length - 1
+	// split string  on spaces or returns into words, remove 'words' of 1 space
+	// return the filtered number of splits (i.e. words)
+	return string.split(/[\n\r\s]+/g).filter((word) => word.length > 0).length;
 }
 
-
+/**
+ * Handle inputs to render the number of words and characters in the HTML
+ */
 function inputHandler() {
-	charCount.textContent = `${textarea.value.length} characters`;
-	wordCount.textContent = `${countWords(textarea.value)} words`
+	charCount.textContent = `${textarea.value.length} ${
+		// if value is singular render correct word
+		textarea.value.length === 1 //
+			? "character"
+			: "characters"
+	}`;
+	wordCount.textContent = `${countWords(textarea.value)} ${
+		// if value is singular render correct word
+		countWords(textarea.value) === 1 //
+			? "word"
+			: "words"
+	}`;
 }
 
 /* ====================================================
