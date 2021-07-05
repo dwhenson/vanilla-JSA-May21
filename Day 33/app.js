0bafc5213583166af97471c458bbe84012639395
dwhenson <dave.henson@gmail.com>
2021-07-05 20:20:49 +0300

    📝 DOC: Add comments and fix to noNote call

--- Day 33/app.js
+++ Day 33/app.js
@@ -15,58 +15,93 @@ const noNotes = "All your notes have been removed.";
 /* ====================================================
    Functions
    ==================================================== */
+/* Helpers
+/* ==================================================== */
 
+/**
+ * Renders the message
+ * @param      {object}  element  The element to insert the message
+ * @param      {sting}  message  The message to render
+ */
 function renderMessage(element, message) {
   setTimeout(function () {
     element.textContent = message;
   }, 10);
 }
 
-// Show a status message
-function showStatus(message) {
+/**
+ * Shows the status of the notes
+ * @param      {object}  insertAfter  The element to append the new HTML element
+ * @param      {string}  message      The message to render
+ */
+function showStatus(insertAfter, message) {
   // Create a notification element
   const notificationElement = document.createElement("div");
   notificationElement.setAttribute("aria-live", "polite");
-  form.append(notificationElement);
+  insertAfter.append(notificationElement);
+  // Add the appropriate message
   renderMessage(notificationElement, message);
-  // Remove it after 2 seconds
+  // Remove it after 3 seconds
   setTimeout(function () {
     notificationElement.remove();
-  }, 2000);
+  }, 3000);
 }
 
-function removeNote() {
+/* Library
+/* ==================================================== */
+
+/**
+ * Removes a note from localStorage
+ * @param      {object}  event    The event
+ * @param      {object}  element  The element to remove remaining value from
+ */
+function removeNote(event, element) {
   event.preventDefault();
+  // Check if there is a note in localStorage
   if (!localStorage.getItem("Note")) {
-    showStatus(noNotes);
+    showStatus(form, noNotes);
     return;
   }
+  // Remove the note and clear the UI
   localStorage.removeItem("Note");
-  showStatus(removedNote);
+  element.value = "";
+  showStatus(form, removedNote);
 }
 
+/**
+ * Saves a note to localStorage
+ * @param      {object}  event    The event
+ * @param      {event}   element  The element containing the value to save
+ */
+function saveNote(event, element) {
+  event.preventDefault();
+  // Check if there is a value entered in the element
+  if (!element.value.trim()) {
+    showStatus(form, emptyNote);
+    return;
+  }
+  // Save the value to localStorage
+  localStorage.setItem("Note", element.value.trim());
+  showStatus(form, savedNote);
+}
+
+/**
+ * Checks localStorage and renders any saved notes
+ */
 function renderSavedNote() {
   if (!localStorage.getItem("Note")) return;
   textarea.value = localStorage.getItem("Note");
 }
 
-function saveNote(event, element) {
-  event.preventDefault();
-  const noteContent = element.value.trim();
-  if (!noteContent) {
-    showStatus(emptyNote);
-    return;
-  }
-  localStorage.setItem("Note", noteContent);
-  showStatus(savedNote);
-}
-
 /* ====================================================
    Inits and Event Listeners
    ==================================================== */
-
+// Check for notes on page load
 renderSavedNote();
-save.addEventListener("click", (event) => {
+// Listen for save or remove note events
+save.addEventListener("click", () => {
   saveNote(event, textarea);
 });
-remove.addEventListener("click", removeNote);
+remove.addEventListener("click", () => {
+  removeNote(event, textarea);
+});
