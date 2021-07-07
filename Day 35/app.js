a3e022812cfea86b052325c6899526ddcc036744
dwhenson <dave.henson@gmail.com>
2021-07-07 17:53:54 +0300

    ✅ ADD: First working solution

--- Day 35/app.js
+++ Day 35/app.js
@@ -3,11 +3,9 @@
    ==================================================== */
 // Elements
 const form = document.querySelector("#notebook");
-const textarea = document.querySelector("#note");
 const save = document.querySelector("#save");
 const remove = document.querySelector("#remove");
 // Messages
-const emptyNote = "You can't save a note with nothing in it. What's the point?";
 const savedNote = "Your note has been saved. You can relax.";
 const removedNote = "Your note has been removed. I hope you'd done with it.";
 const noNotes = "All your notes have been removed.";
@@ -47,6 +45,27 @@ function showStatus(target, message) {
   }, 3000);
 }
 
+/*!
+ * Serialize all form data into an object
+ * (c) 2021 Chris Ferdinandi, MIT License, https://gomakethings.com
+ * @param  {FormData} data The FormData object to serialize
+ * @return {String}        The serialized form data
+ */
+function serialize(data) {
+  const obj = {};
+  for (const [key, value] of data) {
+    if (obj[key] !== undefined) {
+      if (!Array.isArray(obj[key])) {
+        obj[key] = [obj[key]];
+      }
+      obj[key].push(value);
+    } else {
+      obj[key] = value;
+    }
+  }
+  return obj;
+}
+
 /* Library
 /* ==================================================== */
 
@@ -58,13 +77,18 @@ function showStatus(target, message) {
 function removeNote(event, element) {
   event.preventDefault();
   // Check if there is a note in localStorage
-  if (!localStorage.getItem("Note")) {
+  if (!localStorage.getItem("note")) {
     showStatus(form, noNotes);
     return;
   }
   // Remove the note and clear the UI
-  localStorage.removeItem("Note");
-  element.value = "";
+  const data = JSON.parse(localStorage.getItem("note"));
+  for (const datum in data) {
+    // Gets the form element based on the name attribute, and sets its value
+    const element = document.querySelector(`[name='${datum}']`);
+    element.value = "";
+  }
+  localStorage.removeItem("note");
   showStatus(form, removedNote);
 }
 
@@ -75,22 +99,35 @@ function removeNote(event, element) {
  */
 function saveNote(event, element) {
   event.preventDefault();
-  // Check if there is a value entered in the element
-  if (!element.value.trim()) {
-    showStatus(form, emptyNote);
-    return;
+  // Get the form fields and convert to an object
+  const data = new FormData(form);
+  const serializedData = serialize(data);
+  // Check if there is a value entered each form element
+  for (const item in serializedData) {
+    // Checks the form element based on the name attribute
+    const element = document.querySelector(`[name='${item}']`);
+    if (!element.value) {
+      showStatus(form, `Sorry, you have to add some ${item} content`);
+      return;
+    }
   }
-  // Save the value to localStorage
-  localStorage.setItem("Note", element.value.trim());
+  // Save the object to localStorage as a string
+  localStorage.setItem("note", JSON.stringify(serializedData));
   showStatus(form, savedNote);
 }
 
 /**
- * Checks localStorage and renders any saved notes
+ * Checks localStorage and renders any saved notes as needed
  */
 function renderSavedNote() {
-  if (!localStorage.getItem("Note")) return;
-  textarea.value = localStorage.getItem("Note");
+  if (!localStorage.getItem("note")) return;
+  // Gets the note saved in localStorage
+  const data = JSON.parse(localStorage.getItem("note"));
+  for (const datum in data) {
+    // Gets the form element based on the name attribute, and sets its value
+    const element = document.querySelector(`[name='${datum}']`);
+    element.value = data[datum];
+  }
 }
 
 /* ====================================================
@@ -99,9 +136,5 @@ function renderSavedNote() {
 // Check for notes on page load
 renderSavedNote();
 // Listen for save or remove note events
-save.addEventListener("click", () => {
-  saveNote(event, textarea);
-});
-remove.addEventListener("click", () => {
-  removeNote(event, textarea);
-});
+save.addEventListener("click", saveNote);
+remove.addEventListener("click", removeNote);
