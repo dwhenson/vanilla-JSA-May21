c51ae6e8dbc8faadf6ffec8ec1257f82d7d8957b
dwhenson <dave.henson@gmail.com>
2021-07-09 15:57:11 +0300

    📝 DOC: Add comments to functions and sections

--- Day 37/app.js
+++ Day 37/app.js
@@ -1,10 +1,26 @@
+/* ====================================================
+   Variables
+   ==================================================== */
+// Elements
 const app = document.querySelector("#app");
-const errorLocation = "Unable to retrieve your location";
+// Messages
 const errorWeather = "Can't get the weather right now";
-
-const key = "c81e60446f394ac3b6efb4b5c187cafa";
 const endpoint = "https://api.weatherbit.io/v2.0/current?";
+// API
+const key = "c81e60446f394ac3b6efb4b5c187cafa";
+const errorLocation = "Unable to retrieve your location";
 
+/* ====================================================
+   Functions
+   ==================================================== */
+
+/* Helpers
+/* ==================================================== */
+
+/**
+ * Renders the error message to the HTML
+ * @param {string}  message  The error message
+ */
 function error(message) {
   app.innerHTML = `<p>${message}</p`;
 }
@@ -18,6 +34,13 @@ function checkResponse(response) {
   return response.ok ? response.json() : Promise.reject(new Error(response.status));
 }
 
+/* App
+/* ==================================================== */
+
+/**
+ * Renders the data from the APO call to the HTML
+ * @param      {object}  data    The data from the API call
+ */
 function renderWeather(data) {
   console.log(data);
   app.innerHTML = `
@@ -30,18 +53,31 @@ function renderWeather(data) {
   `;
 }
 
+/**
+ * Gets the weather.from the weatherBits API
+ * @param      {string}   latitude   The latitude value
+ * @param      {string}   longitude  The longitude value
+ * @return     {Promise}  The weather from the API call
+ */
 async function getWeather(latitude, longitude) {
   const response = await fetch(`${endpoint}lat=${latitude}&lon=${longitude}&key=${key}`);
   const data = await checkResponse(response);
   renderWeather(data.data[0]);
 }
 
+/**
+ * Assigns lat/long values to variables and calls getWeather
+ * @param      {object}  position  The position object from the geolocation API
+ */
 function successLatLong(position) {
   const latitude = position.coords.latitude;
   const longitude = position.coords.longitude;
   getWeather(latitude, longitude).catch(error(errorWeather));
 }
 
+/**
+ * Gets the lat long.from the geolocation API
+ */
 function getLatLong() {
   if (!navigator.geolocation) {
     app.textContent = "Geolocation is not supported by your browser";
@@ -52,4 +88,7 @@ function getLatLong() {
   }
 }
 
+/* ====================================================
+   Inits and Event Listeners
+   ==================================================== */
 getLatLong();
