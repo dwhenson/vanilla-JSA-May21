9948fb37d249d5f05414fffdc376199dd39b316e
dwhenson <dave.henson@gmail.com>
2021-07-20 09:05:21 +0300

    📝 DOC:  Function comments

--- Day 45/index.js
+++ Day 45/index.js
@@ -1,3 +1,7 @@
+/**
+ * Adds user defined units of time to an orginal time, and formats the result
+ * @return     {objects}  Values from utility functions
+ */
 let stamp = (function () {
   // Times in milliseconds
   let times = {
@@ -8,22 +12,30 @@ let stamp = (function () {
     years: 1000 * 60 * 60 * 24 * 365,
   };
 
+  /**
+   * Adds a number of units of time to a timestap
+   * @param      {number}  timestamp  The timestamp
+   * @param      {number}  number     The number of units to add
+   * @return     {number}  The combined timestamp and units * milliseconds
+   */
   function addHours(timestamp, number) {
     return timestamp + number * times.hours;
   }
-
   function addDays(timestamp, number) {
     return timestamp + number * times.days;
   }
-
   function addWeeks(timestamp, number) {
     return timestamp + number * times.weeks;
   }
-
   function addYears(timestamp, number) {
     return timestamp + number * times.years;
   }
 
+  /**
+   * Formats a timestamp and returns a formated date
+   * @param      {number}  timestamp  The timestamp
+   * @return     {string}    The formatted date string
+   */
   function getDate(timestamp) {
     return new Date(timestamp).toLocaleString(navigator.language, {
       hour12: true,
@@ -34,6 +46,9 @@ let stamp = (function () {
 
   return { addHours, addDays, addWeeks, addYears, getDate };
 })();
+
+/* Inits
+/* ==================================================== */
 
 let now = new Date().getTime();
 let threeWeeks = stamp.addDays(now, 3);
